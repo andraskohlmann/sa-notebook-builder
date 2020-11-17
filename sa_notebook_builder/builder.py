c9300fd0869c5d8a5fecae4e6d32751ea5d43297
Kohlmann András <metuoku@outlook.com>
2020-11-18 00:54:58 +0100

    :sparkles: Start markdown support

--- sa_notebook_builder/builder.py
+++ sa_notebook_builder/builder.py
@@ -1,9 +1,22 @@
-import json
+import nbformat as nbf
 
 
 class Builder:
-    @staticmethod
-    def render(filename):
-        with open(filename, "w+") as f:
-            nb = {"cells": []}
-            f.write(f'{json.dumps(nb)}')
+    def __init__(self):
+        self._inport_cells = []
+        self._cells = []
+
+    def render(self, filename: str):
+        nb = nbf.v4.new_notebook()
+        nb["cells"] = self._inport_cells + self._cells
+        nbf.write(nb, filename)
+
+    def add_markdown(self, markdown: str):
+        cell = nbf.v4.new_markdown_cell(markdown)
+        self._cells.append(cell)
+
+
+if __name__ == '__main__':
+    b = Builder()
+    b.add_markdown("## Complex z")
+    b.render('main.ipynb')
--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,4 +1,5 @@
 import os
+from unittest.mock import ANY
 
 import nbconvert
 import nbformat
@@ -24,3 +25,26 @@ def test_builder_creates_legit_ipynb(tmpdir):
     b.render(os.path.join(tmpdir, 'empty.ipynb'))
 
     run_notebook(os.path.join(tmpdir, 'empty.ipynb'))
+
+
+def test_add_markdown_adds_markdown(tmpdir):
+    b = Builder()
+    md = """
+        # Title
+        ## Subtitle
+        Text
+    """
+    filename = os.path.join(tmpdir, 'md.ipynb')
+
+    b.add_markdown(md)
+    b.render(filename)
+
+    rendered_nb = nbformat.read(filename, as_version=4)
+    assert len(rendered_nb.cells) == 1
+
+    expected_cell = {
+        "cell_type": "markdown",
+        "metadata": ANY,
+        "source": md,
+    }
+    assert rendered_nb.cells[0] == expected_cell
